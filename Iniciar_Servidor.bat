521a98c526b5f0cbc3b9a3db2b9b3ead917985c9
Francisco Diaz <frandiaz838@gmail.com>
2026-06-08 13:25:51 -0300

    borramos archivo innecesario .bat

--- Iniciar_Servidor.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set PYTHONUTF8=1
-cd /d "%~dp0"
-echo [DIArize] Iniciando servidor en http://localhost:8765 ...
-echo [DIArize] Deja esta ventana abierta mientras usas el cliente C++.
-echo.
-"%~dp0venv\Scripts\python.exe" -X utf8 "%~dp0Transcriptor\servidor.py"
-pause
